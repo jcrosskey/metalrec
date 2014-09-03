7e8f9c913f5e12bb86e66ab9d9fa3af030aef537
JJ Chai <chaij@umail.iu.edu>
2014-09-02 22:50:14 -0400

    remove two read_and_process functions without samread class, better control of verbose output

--- src/tmp.py
+++ src/tmp.py
@@ -86,6 +86,7 @@ for k, z in bp_pos_dict.items():
 ## test of newly added functions in samread class ##
 import samread
 import metalrec_lib
+from numpy import *
 
 maxSub=3
 maxIns=3
@@ -110,14 +111,16 @@ r1 = samread.SamRead(a) # SamRead object
 samIn.close()
 
 
-ref_bps, ref_ins_dict, readinfo = metalrec_lib.read_and_process_sam_samread(samfile, rseq, maxSub=10, maxDel=100, maxIns=100, maxSubRate=0.1, outsamFile="/Users/cjg/Work/PacBio/metalrec/test/bbmap_red.sam", outFastaFile="/Users/cjg/Work/PacBio/metalrec/test/good_reads.fasta")
+ref_bps, ref_ins_dict, readinfo = metalrec_lib.read_and_process_sam_samread(samfile, rseq, maxSub=10, maxDel=100, maxIns=100, maxSubRate=0.1, outsamFile="/Users/cjg/Work/PacBio/metalrec/test/a.sam", outFastaFile="/Users/cjg/Work/PacBio/metalrec/test/good_reads.fasta")
 good_regions = metalrec_lib.get_good_regions(ref_bps, rseq, minGoodLen=1, minCV=3)
-poly_bps, poly_ins, consensus_bps, consensus_ins, cvs = metalrec_lib.get_poly_pos(ref_bps, ref_ins_dict, good_regions[0])
-newSeq, bp_pos_dict, ins_pos_dict = metalrec_lib.ref_extension(poly_bps, poly_ins, consensus_bps, consensus_ins, rseq, print_width=70)
+poly_bps, poly_ins, consensus_bps, consensus_ins, cvs = metalrec_lib.get_poly_pos(ref_bps, ref_ins_dict, good_regions[0][0])
+newSeq, bp_pos_dict, ins_pos_dict = metalrec_lib.ref_extension(poly_bps, poly_ins, consensus_bps, consensus_ins, rseq, print_width=70, verbose=True)
 poly_bps_ext, poly_ins_ext, consensus_bps_ext, consensus_ins_ext = metalrec_lib.update_pos_info(poly_bps, poly_ins, consensus_bps, consensus_ins, bp_pos_dict, ins_pos_dict)
 type_array, ext_region = metalrec_lib.make_type_array(poly_bps_ext, poly_ins_ext, consensus_bps_ext, consensus_ins_ext)
 #consensus_array = metalrec_lib.make_ref_array(consensus_bps_ext, consensus_ins_ext, type_array,ext_region)
 read_array1d = metalrec_lib.make_read_array1d(readinfo.keys()[0], bp_pos_dict, ins_pos_dict, type_array, poly_bps_ext, poly_ins_ext, consensus_bps_ext, consensus_ins_ext) 
 read_array, read_counts = metalrec_lib.make_read_array(readinfo, bp_pos_dict, ins_pos_dict, type_array, poly_bps_ext, poly_ins_ext, consensus_bps_ext, consensus_ins_ext)
-ref_new, gap = metalrec_lib.fill_gap(read_array, "/Users/cjg/Work/PacBio/metalrec/test/good_reads.fasta", "/Users/cjg/Work/PacBio/metalrec/test", readinfo)
+ref_new = metalrec_lib.fill_gap(read_array, "/Users/cjg/Work/PacBio/metalrec/test/good_reads.fasta", "/Users/cjg/Work/PacBio/metalrec/test", readinfo, verbose=False)
 ref_new_short, ref_new_long = metalrec_lib.array_to_seq(ref_new)
+ref0, tot_gap, Cvec = metalrec_lib.greedy_fill_gap(read_array, ref0=None, verbose=False)
+new_ref = metalrec_lib.fill_gap(read_array, outFastaFile="/Users/cjg/Work/PacBio/metalrec/test/good_reads.fasta", outDir=None, readinfo=readinfo, verbose=True)
