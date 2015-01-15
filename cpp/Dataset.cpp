5578c43f9ddb2d545644bff40bd660504d58a2c4
JJ Chai <chaij@umail.iu.edu>
2015-01-15 16:13:10 -0500

    No longer try to calculate the stream size, does not work on empty stdin

--- cpp/Dataset.cpp
+++ cpp/Dataset.cpp
@@ -142,73 +142,70 @@ Dataset::Dataset(FILE * inputSamStream, UINT64 minOverlap, const float & indelRa
 	UINT64 goodReads = 0, badReads = 0;
 	/** get reads from sam file **/
 	string line = "";
-	char buffer[1000];
-	long streamSize;
-	fseek(inputSamStream, 0, SEEK_SET);
-	streamSize = ftell(inputSamStream);
-	FILE_LOG(logDEBUG) << "Stream size is: " << streamSize;
-	if (streamSize != 0)
+	char buffer[BUFFER_SIZE];
+//	long streamSize;
+//	fseek(inputSamStream, 0, SEEK_SET);
+//	streamSize = ftell(inputSamStream);
+//	FILE_LOG(logDEBUG) << "Stream size is: " << streamSize;
+	while(1)
 	{
-		while(!feof(inputSamStream))
+		if(fgets(buffer, BUFFER_SIZE, inputSamStream)==NULL)
 		{
-			if(fgets(buffer, 1000, inputSamStream)!=NULL) /* Get buffer of size 1000 */
-			{
-				line += buffer;
-			}
-			else {
-				perror("Error in function fgets");
-				break;
-			}
-			if (line.at(line.size() - 1) == '\n')        /* A whole line has been read, processing it */
-			{
-				line.erase(line.size()-1, 1);        /* Delete last character from line, which is EOL */
-				FILE_LOG(logDEBUG4) << "Line: " << line;
-				if (line[0] == '@')
-				{
-					FILE_LOG(logDEBUG3) << "header line";
-					if ( line.substr(0,3).compare("@SQ") ==0) /* SQ line */
-					{
-						size_t LNPos = line.find("LN:");
-						size_t nextTabPos = line.find("\t", LNPos+3);
-						PacBioReadLength = Utils::stringToUnsignedInt(line.substr(LNPos+3,nextTabPos-LNPos-3)); /* length of the PacBio read */
-						LNPos = line.find("SN:");
-						nextTabPos = line.find("\t", LNPos+3);
-						PacBioReadName = line.substr(LNPos+3,nextTabPos-LNPos-3);
-					}
-				}
-				else	// not header line, alignment record
-				{
-					FILE_LOG(logDEBUG4) << "Read line with length: " << line.length();
-					if (line.length() !=0)
-					{
-						Read *r = new Read(line);
-						if ( PacBioReadName.length() == 0 )
-							PacBioReadName = r->getRefName();
-						FILE_LOG(logDEBUG4) << "Scanned read: " << r->getReadName();
-						if ( r->isReadGood(indelRate, subRate) && testRead(r->getDnaStringForward()))
-						{
-							UINT32 len = r->getReadLength();
-							if (len > longestReadLength)
-								longestReadLength = len;
-							if (len < shortestReadLength)
-								shortestReadLength = len;
-							reads->push_back(r);
-							numberOfReads++;
-							goodReads++;
-						}
-						else
-						{
-							badReads++;
-							if (!(r->isReadGood(indelRate, subRate)))
-								FILE_LOG(logDEBUG3) << "Read " << r->getReadName() << " has too many errors";
-							else
-								FILE_LOG(logDEBUG3) << "Read is not valid";
-						}
-					}
-				}
-				line = "";              /* Set line to empty again */
-			}
+			FILE_LOG(logERROR) << __FILE__;
+			perror("Encountered in function fgets");
+			break;
 		}
+		line += buffer;
+		if (line.at(line.size() - 1) == '\n')        /* A whole line has been read, processing it */
+		{
+			line.erase(line.size()-1, 1);        /* Delete last character from line, which is EOL */
+			FILE_LOG(logDEBUG4) << "Line: " << line;
+			if (line[0] == '@')
+			{
+				FILE_LOG(logDEBUG3) << "header line";
+				if ( line.substr(0,3).compare("@SQ") ==0) /* SQ line */
+				{
+					size_t LNPos = line.find("LN:");
+					size_t nextTabPos = line.find("\t", LNPos+3);
+					PacBioReadLength = Utils::stringToUnsignedInt(line.substr(LNPos+3,nextTabPos-LNPos-3)); /* length of the PacBio read */
+					LNPos = line.find("SN:");
+					nextTabPos = line.find("\t", LNPos+3);
+					PacBioReadName = line.substr(LNPos+3,nextTabPos-LNPos-3);
+				}
+			}
+			else	// not header line, alignment record
+			{
+				FILE_LOG(logDEBUG4) << "Read line with length: " << line.length();
+				if (line.length() !=0)
+				{
+					Read *r = new Read(line);
+					if ( PacBioReadName.length() == 0 )
+						PacBioReadName = r->getRefName();
+					FILE_LOG(logDEBUG4) << "Scanned read: " << r->getReadName();
+					if ( r->isReadGood(indelRate, subRate) && testRead(r->getDnaStringForward()))
+					{
+						UINT32 len = r->getReadLength();
+						if (len > longestReadLength)
+							longestReadLength = len;
+						if (len < shortestReadLength)
+							shortestReadLength = len;
+						reads->push_back(r);
+						numberOfReads++;
+						goodReads++;
+					}
+					else
+					{
+						badReads++;
+						if (!(r->isReadGood(indelRate, subRate)))
+							FILE_LOG(logDEBUG3) << "Read " << r->getReadName() << " has too many errors";
+						else
+							FILE_LOG(logDEBUG3) << "Read is not valid";
+					}
+				}
+			}
+			line = "";              /* Set line to empty again */
+		}
+	}
 	FILE_LOG(logINFO) << "Shortest read length: " << setw(5) << shortestReadLength;
 	FILE_LOG(logINFO) << "Longest read length: " << setw(5) << longestReadLength;
 	FILE_LOG(logINFO) << "Number of good reads: " << setw(5) << goodReads;
@@ -217,11 +214,6 @@ Dataset::Dataset(FILE * inputSamStream, UINT64 minOverlap, const float & indelRa
 	sortReads();
 	removeDupicateReads();	// Remove duplicated reads for the dataset.
 	numberOfNonContainedReads = numberOfUniqueReads;
-	}
-	else
-	{
-		Utils::exitWithError("Input sam stream is empty, quit..\n");
-	}
 	CLOCKSTOP;
 }
 
